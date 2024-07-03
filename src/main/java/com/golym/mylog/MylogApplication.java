82ba74b212a3ab4bd8317319f1ce8efcf6555e82
vvxxxxk <vvxxxxk@gmail.com>
2024-07-04 04:37:03 +0900

    chore: initial project

---
+++ src/main/java/com/golym/mylog/MylogApplication.java
@@ -0,0 +1,13 @@
+package com.golym.mylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylogApplication.class, args);
+	}
+
+}
